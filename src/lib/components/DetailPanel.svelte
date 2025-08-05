b9da18f5e5154933b5b01c1ec9f6406458cd59c7
Mariel de la Garza <mariel.code@gmail.com>
2025-08-05 18:51:48 -0400

    Move map to the top of detail panel

--- src/lib/components/DetailPanel.svelte
+++ src/lib/components/DetailPanel.svelte
@@ -1,19 +1,18 @@
-<!--This will hold the data visualization for a selected element -->
 <script>
-  import ImportRelianceChart from '$lib/components/ImportRelianceChart.svelte';
-  import ImportValuesChart from '$lib/components/ImportValuesChart.svelte';
-  import Map from '$lib/components/Map.svelte';
+	import ImportRelianceChart from '$lib/components/ImportRelianceChart.svelte';
+	import ImportValuesChart from '$lib/components/ImportValuesChart.svelte';
+	import Map from '$lib/components/Map.svelte';
 </script>
 
 <div class="detail-panel">
-  <h2>Detail Panel</h2>
-  <ImportRelianceChart />
-  <ImportValuesChart />
-  <Map />
+	<h2>Detail Panel</h2>
+	<Map />
+	<ImportRelianceChart />
+	<ImportValuesChart />
 </div>
 
 <style>
-  .detail-panel {
-    outline: 1px solid red;
-  }
+	.detail-panel {
+		outline: 1px solid red;
+	}
 </style>
