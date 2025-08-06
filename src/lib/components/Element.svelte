a4f9241dcbbcb5e59e5949bcd3e30c38d725e72f
Mariel de la Garza <mariel.code@gmail.com>
2025-08-05 21:33:38 -0400

    Update data.js to use TRUE from CSV and update element colors as test

--- src/lib/components/Element.svelte
+++ src/lib/components/Element.svelte
@@ -1,9 +1,14 @@
 <!-- This component represents a single element in the periodic table -->
 <script>
 	export let element;
+
+	let classes = 'element';
+	if (element['2022_doi_list']) classes += ' doi';
+
+  console.log('Element:', element);
 </script>
 
-<div class="element">
+<div class={classes}>
 	<div class="number">{element.atomic_number}</div>
 	<div class="symbol">{element.symbol}</div>
 </div>
@@ -32,33 +37,37 @@
 		font-size: 1.5rem;
 	}
 
-  @media (max-width: 1900px) {
-    .element {
-      width: 3rem;
-      height: 3rem;
-    }
+	.doi {
+		background-color: #dd9933; /* whatever highlight color you want */
+		color: white;
+	}
 
-    .number {
-      font-size: 0.8rem;
-    }
+	@media (max-width: 1900px) {
+		.element {
+			width: 3rem;
+			height: 3rem;
+		}
 
-    .symbol {
-      font-size: 1.2rem;
-    }
-  }
-  @media (max-width: 1500px) {
-    .element {
-      width: 3rem;
-      height: 3rem;
-    }
+		.number {
+			font-size: 0.8rem;
+		}
 
-    .number {
-      font-size: 0.7rem;
-    }
+		.symbol {
+			font-size: 1.2rem;
+		}
+	}
+	@media (max-width: 1500px) {
+		.element {
+			width: 3rem;
+			height: 3rem;
+		}
 
-    .symbol {
-      font-size: 1rem;
-    }
-  }
+		.number {
+			font-size: 0.7rem;
+		}
 
+		.symbol {
+			font-size: 1rem;
+		}
+	}
 </style>
