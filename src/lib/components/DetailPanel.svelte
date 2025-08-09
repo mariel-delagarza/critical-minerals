dc362b219b46f4a326f75ecc3806dd7df65bdd48
Mariel de la Garza <mariel.code@gmail.com>
2025-08-09 19:43:26 -0400

    Update text on detail panel

--- src/lib/components/DetailPanel.svelte
+++ src/lib/components/DetailPanel.svelte
@@ -4,23 +4,18 @@
 	import Map from '$lib/components/Map.svelte';
 	import { selectedElement } from '$lib/stores/selectedElement';
 
-	$: text = '';
-	if ($selectedElement?.name === 'Aluminum') {
-		text = $selectedElement.notes;
-	}
-
+	$: text = $selectedElement.notes;
 	$: name = $selectedElement?.name;
 
+	/* Put together statement about what lists element is on*/
 	let doi_label = 'Department of Interior';
 	let doe_label = 'Department of Energy';
 	let dla_label = 'Defense Logistics Agency';
-
 	$: agencies = [
 		$selectedElement?.['2022_doi_list'] && 'Department of the Interior',
 		$selectedElement?.doe_critical_mineral && 'Department of Energy',
 		$selectedElement?.dla_materials_of_interest && 'Defense Logistics Agency'
 	].filter(Boolean);
-
 	function makeListStatement(agencies) {
 		if (agencies.length == 1) {
 			return `list of the ${agencies[0]}`;
@@ -30,14 +25,24 @@
 			return `lists of the ${agencies[0]}, the ${agencies[1]}, and the ${agencies[2]}`;
 		}
 	}
-
 	$: listStatement = makeListStatement(agencies);
 </script>
 
 <div class="detail-panel">
 	<h2>{name}</h2>
-	<p>{name} is on the critical minerals {listStatement}.</p>
-
+	<p>{name} is on the critical minerals {listStatement}. {text}</p>
+	{#if $selectedElement}
+		{#each Object.entries($selectedElement.materials) as [materialName, materialData]}
+			{#if materialData.applications}
+				<h3>{materialName}</h3>
+				<ul>
+					{#each materialData.applications.split(';').map((a) => a.trim()) as app}
+						<li>{app}</li>
+					{/each}
+				</ul>
+			{/if}
+		{/each}
+	{/if}
 	{#if $selectedElement}
 		<!-- <Map element={$selectedElement} /> -->
 		<div id="charts">
