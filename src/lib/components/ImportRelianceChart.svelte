bac3daeb0a7ddf399e604476eb5aa71e86379521
Mariel de la Garza <mariel.code@gmail.com>
2025-08-09 14:26:54 -0400

    working import reliance chart

--- src/lib/components/ImportRelianceChart.svelte
+++ src/lib/components/ImportRelianceChart.svelte
@@ -1,27 +1,87 @@
-<!-- This will be a line chart comparing net import reliance over time -->
 <script>
 	import { onMount } from 'svelte';
 	import Highcharts from 'highcharts';
 
-  export let element;
+	export let element;
 
-  $: console.log('ImportRelianceChart got element: ', element)
+	// $: console.log('ImportRelianceChart got element: ', element)
 
+	// y-axis: country names
+	let categories = [];
+	// materials - 1 per series
+	let series = [
+		{
+			name: '', // material name
+			data: [] // material import numbers
+		}
+	];
+
+	// for (const [materialName, materialData] of Object.entries(element.materials)) {
+	// 	console.log('Material:', materialName);
+	// 	// console.log('Data:', materialData);
+
+	// 	// Example: get imports
+	// 	if (materialData.imports) {
+	// 		for (const [country, percent] of Object.entries(materialData.imports)) {
+	// 			// console.log(`  ${country}: ${percent}`);
+	//       categories.push(country)
+	// 		}
+	// 	}
+	// }
+	// console.log(categories)
+
+	// 1) Build categories (unique country list) and series whenever `element` changes
+	$: if (element?.materials) {
+		// unique countries across all materials
+		const countrySet = new Set();
+		for (const [, materialData] of Object.entries(element.materials)) {
+			if (materialData?.imports) {
+				for (const country of Object.keys(materialData.imports)) {
+					countrySet.add(country);
+				}
+			}
+		}
+		categories = Array.from(countrySet);
+
+		// 2) Build series: one per material, aligned to categories
+		series = Object.entries(element.materials).map(([materialName, materialData]) => {
+			const imports = materialData.imports || {};
+			const data = categories.map((country) =>
+				// use 0 if missing; use null if you want gaps instead of zeros
+				typeof imports[country] === 'number' ? imports[country] : 0
+			);
+			return { name: materialName, data };
+		});
+
+		console.log(categories);
+		console.log(series);
+	}
+
+	// $: console.log(getActiveMaterial(element));
 	onMount(() => {
 		Highcharts.chart('reliance-container', {
 			chart: {
-				type: 'bar'
+				type: 'bar',
+				backgroundColor: '#eee6d8'
 			},
 			title: {
-				text: 'Corn vs wheat estimated production for 2023'
-			}, 
+				text: 'Countries imported from'
+			},
+			plotOptions: {
+				bar: {
+					pointPadding: 0.2,
+					borderWidth: 0,
+					borderRadius: 0
+				}
+			},
+			credits: {
+				enabled: false
+			},
 			subtitle: {
-				text:
-					'Source: <a target="_blank" ' +
-					'href="https://www.indexmundi.com/agriculture/?commodity=corn">indexmundi</a>'
+				text: ''
 			},
 			xAxis: {
-				categories: ['USA', 'China', 'Brazil', 'EU', 'Argentina', 'India'],
+				categories: categories,
 				crosshair: true,
 				accessibility: {
 					description: 'Countries'
@@ -30,41 +90,20 @@
 			yAxis: {
 				min: 0,
 				title: {
-					text: '1000 metric tons (MT)'
-				}
-			},
-			tooltip: {
-				valueSuffix: ' (1000 MT)'
-			},
-			plotOptions: {
-				column: {
-					pointPadding: 0.2,
-					borderWidth: 0
-				}
-			},
-			series: [
-				{
-					name: 'Corn',
-					data: [387749, 280000, 129000, 64300, 54000, 34300]
+					text: 'Percentage of Imports'
 				},
-				{
-					name: 'Wheat',
-					data: [45321, 140000, 10000, 140500, 19500, 113500]
-				}
-			]
+				opposite: true
+			},
+			tooltip: {},
+			series: series
 		});
 	});
 </script>
 
 <div id="import-reliance-chart">
-	<strong>Import Reliance Chart</strong>
 
 	<figure class="highcharts-figure">
 		<div id="reliance-container"></div>
-		<p class="highcharts-description">
-			A basic column chart comparing estimated corn and wheat production in some countries. The
-			chart is making use of the axis crosshair feature, to highlight the hovered country.
-		</p>
 	</figure>
 </div>
 
