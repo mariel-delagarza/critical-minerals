bf6c4e37a0c7038a183fc1db9361a71e87d1f1cd
Mariel de la Garza <mariel.code@gmail.com>
2025-08-20 14:35:34 -0400

    Update additional sources

--- src/lib/components/Sources.svelte
+++ src/lib/components/Sources.svelte
@@ -4,7 +4,7 @@
 		<ol>
 			<li id="footnote1">
 				<a href="https://pubs.usgs.gov/periodicals/mcs2025/mcs2025.pdf"
-					>Mineral Commodity Summaries 2025 (USGS)</a
+					>Mineral Commodity Summaries 2025. U.S. Geological Survey. March 2025.</a
 				>
 			</li>
 			<li id="footnote2">
@@ -34,10 +34,24 @@
       <li id="footnote8">
         <a href="https://www.federalregister.gov/documents/2023/08/04/2023-16611/notice-of-final-determination-on-2023-doe-critical-materials-list">Notice of Final Determination on 2023 DOE Critical Materials List, 88 Fed. Reg. 51792 (Aug. 4, 2023).</a>
       </li>
-      <li id="footnote9"><a href="https://www.dla.mil/Strategic-Materials/About/">"About Strategic Materials." Defense Logistics Agency. Accessed 20 August 2025.</a></li>
-      <li id="footnote10"><a href="https://www.dla.mil/Strategic-Materials/Materials/">"Materials of Interest." Defense Logistics Agency. Accessed 20 August 2025.</a></li>
+      <li id="footnote9"><a href="https://www.dla.mil/Strategic-Materials/About/">"About Strategic Materials." Defense Logistics Agency. Accessed 20 Aug. 2025.</a></li>
+      <li id="footnote10"><a href="https://www.dla.mil/Strategic-Materials/Materials/">"Materials of Interest." Defense Logistics Agency. Accessed 20 Aug. 2025.</a></li>
+      <li id="footnote 11"><a href="https://www.usgs.gov/news/featured-story/risk-and-reliance-us-economy-and-mineral-resources ">"Net import reliance refers to the percentage of a mineral commodity used by the United States that must be imported from another country." (Risk and Reliance: "The U.S. Economy and Mineral Resources." U.S. Geological Survey. 12 Apr. 2017) </a></li>
 		</ol>
 	</div>
+  <h2>Additional Reading</h2>
+  <ul>
+    <li><a href="https://www.energy.gov/cmm/what-are-critical-materials-and-critical-minerals">"What are Critical Materials and Critical Minerals?" (U.S. Department of Energy)</a></li>
+    <li><a href="https://www.energy.gov/sites/default/files/2023-07/doe-critical-material-assessment_07312023.pdf">Critical Materials Asssesment. U.S. Department of Energy. July 2023.</a></li>
+    <li><a href="https://chem.libretexts.org/Bookshelves/Introductory_Chemistry/Introduction_to_General_Chemistry_%28Malik%29/02%3A_Elements/2.04%3A_The_periodic_table">The periodic table. LibreTexts Chemistry. Accessed 20 Aug. 2025.</a></li>
+    <li><a href="https://www.usgs.gov/centers/national-minerals-information-center/state-minerals-statistics-and-information">State Minerals Statistics and Information. U.S. Geological Survey. Accessed 20 Aug. 2025.</a></li>
+    <li><a href="https://www.energy.gov/sites/default/files/2025-05/doe-intrinsic-role-of-coal-in-achieving-steel-dominance.pdf">The Intrinsic Role of Coal in Achieving Steel Dominance. U.S. Department of Energy. Accessed 20 Aug. 2025.</a></li>
+    <li><a href="https://www.energy.gov/sites/default/files/2025-05/notice-metallurgical-coal-steelmaking-critical-materials-list.pdf">Notice: Metallurgical Coal added to DOE Critical Material List. U.S. Department of Energy. 22 May 2025.</a></li>
+    <li><a href="https://unu.edu/merit/news/what-are-critical-minerals-and-why-are-they-so-important"> Valverde, Jorge. "What Are Critical Minerals, and Why Are They So Important?" UNU-MERIT, 2 May 2024.</a></li>
+    <li><a href="https://bipartisanpolicy.org/blog/which-minerals-are-critical-its-more-complicated-than-you-think/"> Elizalde, Daniel, et al. "Which Minerals are Critical? It's More Complicated Than You Think." Bipartisan Policy Center, 6 Aug. 2024.</a></li>
+    <li><a href="https://www.usgs.gov/faqs/what-a-critical-mineral">"What is a critical mineral?" U.S. Geological Survey. Accessed 20 Aug. 2025.</a></li>
+    <li><a href="https://www.congress.gov/crs-product/R47982">Rowan, Linda R. "Critical Mineral Resources: National Policy and Critical Minerals List." Congressional Research Service. 17 Jul. 2025.</a></li>
+  </ul>
 </section>
 
 <style>
